86da81d0e8bcd11d3c28ecdbf7bc96374a98c5c0
kubicek-skoleni <kubicek.brno@gmail.com>
2021-11-22 10:41:03 +0100

    zadani prvniho ukolu

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -2,4 +2,8 @@
 
 
 var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
 var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+// strings - pomocí LINQu vytvořte nové pole kde jsou všechna slova uppercase
+
