0ab690d01214055345437b10b20956aab1d520b2
kubicek-skoleni <kubicek.brno@gmail.com>
2021-11-22 10:35:21 +0100

    dve pole, numbers, strings

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,1 +1,5 @@
 ﻿Console.WriteLine("Hello, World!");
+
+
+var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
