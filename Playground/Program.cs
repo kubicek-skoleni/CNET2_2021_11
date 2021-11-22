eaabe767de22b63cce1947931e730c75df450423
kubicek-skoleni <kubicek.brno@gmail.com>
2021-11-22 11:23:10 +0100

    LINQ ukol 3 - dve pole, transformace

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -2,19 +2,22 @@
 
 var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 
-var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+var strings = new[] { "zero", "one", "two", "three", 
+                        "four", "five", "six", "seven",
+                        "eight", "nine" };
 
 // 1 strings - pomocí LINQu vytvořte nové pole kde jsou všechna slova uppercase
-var result = strings.Select(x => x.ToUpper());
+//var result = strings.Select(x => x.ToUpper());
 
 // 2 numbers - zjiste pomoci LINQu jestli pole obsahuje pouze suda cisla
-bool isOnlyEvenNumbers = numbers.All(x => x % 2 == 0);
+//bool isOnlyEvenNumbers = numbers.All(x => x % 2 == 0);
+//global::System.Console.WriteLine($"jsou vsechna cisla suda: {isOnlyEvenNumbers}");
 
-global::System.Console.WriteLine($"jsou vsechna cisla suda: {isOnlyEvenNumbers}");
+// 3 - vypište čísla v poli numbers jako slova - LINQ
+var result = numbers.Select(x => strings[x]);
 
-//PrintList(result.ToList());
 
-
+PrintList(result.ToList());
 
 static void PrintList(List<string> listToPrint)
 {
