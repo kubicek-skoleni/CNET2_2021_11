46b238f310a0d4af87fc3eb6697bc5c56538d55b
urza <82667968+kubicek-skoleni@users.noreply.github.com>
2021-11-22 21:59:55 +0100

    progressbars and usercontrolls, manually adding to stack

--- WPFTextGUI/MainWindow.xaml.cs
+++ WPFTextGUI/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFTextGUI.ViewModels;
 
 namespace WPFTextGUI
 {
@@ -28,21 +29,33 @@ namespace WPFTextGUI
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            var bookdir = @"C:\Users\Student\source\repos\CNET2\Books";
+            var bookdir = @"C:\PROJECTS\IctPro\CNET2-2021-11\CNET2\Books";
+
+            List<WordInfo> wordInfos = new();
 
             foreach (var file in GetFilesFromDir(bookdir))
             {
                 var dict = TextTools.TextTools.FreqAnalysis(file);
                 var top10 = TextTools.TextTools.GetTopWords(10, dict);
                 var fi = new FileInfo(file);
-
-                txbInfo.Text += fi.Name + Environment.NewLine;
+                stckWords.Children.Add(new TextBlock() { Text = fi.Name, FontSize = 25 });
+                //txbInfo.Text += fi.Name + Environment.NewLine;
                 foreach (var kv in top10)
                 {
-                    txbInfo.Text += $"{kv.Key}: {kv.Value} {Environment.NewLine}";
+                    //txbInfo.Text += $"{kv.Key}: {kv.Value} {Environment.NewLine}";
+                    stckWords.Children.Add(new WordLineInfo(kv.Key));
                 }
-                txbInfo.Text += Environment.NewLine;
+                //txbInfo.Text += Environment.NewLine;
+
+                wordInfos.AddRange(top10.Select(x => new WordInfo() { Word = x.Key, Count = x.Value}));
+
+                stckWords.Children.Add(new WordLineInfo());
+
             }
+            stckWords.Children.Add(new WordLineInfo());
+             
+            //itemswords.ItemsSource = wordInfos;
+
         }
 
         static IEnumerable<string> GetFilesFromDir(string dir)
