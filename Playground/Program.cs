9451b3c88dc04122cbd1123dc6128b4a0da475cb
kubicek-skoleni <kubicek.brno@gmail.com>
2021-11-22 10:46:07 +0100

    reseni ukolu 1

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,9 +1,14 @@
 ﻿Console.WriteLine("Hello, World!");
 
-
 var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 
 var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
 // strings - pomocí LINQu vytvořte nové pole kde jsou všechna slova uppercase
 
+var result = strings.Select(x => x.ToUpper());
+
+foreach(var item in result)
+{
+    Console.WriteLine(item);
+}
