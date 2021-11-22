e2f04d11b38af8adb130487c96fc5447c7b063fd
kubicek-skoleni <kubicek.brno@gmail.com>
2021-11-22 16:24:35 +0100

    wpf analyza knih, nezobrazuje..

--- WPFTextGUI/MainWindow.xaml.cs
+++ WPFTextGUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,30 @@ namespace WPFTextGUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var bookdir = @"C:\Users\Student\source\repos\CNET2\Books";
+
+            foreach (var file in GetFilesFromDir(bookdir))
+            {
+                var dict = TextTools.TextTools.FreqAnalysis(file);
+                var top10 = TextTools.TextTools.GetTopWords(10, dict);
+                var fi = new FileInfo(file);
+
+                txbInfo.Text += fi.Name + Environment.NewLine;
+                foreach (var kv in top10)
+                {
+                    txbInfo.Text += $"{kv.Key}: {kv.Value} {Environment.NewLine}";
+                }
+                txbInfo.Text = Environment.NewLine;
+            }
+        }
+
+        static IEnumerable<string> GetFilesFromDir(string dir)
+        {
+            return Directory.EnumerateFiles(dir);
         }
     }
 }
