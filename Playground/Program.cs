af98bbe0275a792415f3fddb1fc3a957bf30811b
kubicek-skoleni <kubicek.brno@gmail.com>
2021-11-24 10:10:34 +0100

    tasky v konzolove aplikaci

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -2,72 +2,24 @@
 
 Console.WriteLine("Hello, World!");
 
-var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-
-var strings = new[] { "zero", "one", "two", "three", 
-                        "four", "five", "six", "seven",
-                        "eight", "nine" };
-
-// 1 strings - pomocí LINQu vytvořte nové pole kde jsou všechna slova uppercase
-//var result = strings.Select(x => x.ToUpper());
-
-// 2 numbers - zjiste pomoci LINQu jestli pole obsahuje pouze suda cisla
-//bool isOnlyEvenNumbers = numbers.All(x => x % 2 == 0);
-//global::System.Console.WriteLine($"jsou vsechna cisla suda: {isOnlyEvenNumbers}");
-
-// 3 - vypište čísla v poli numbers jako slova - LINQ
-// var result = numbers.Select(x => strings[x]);
-
-// 4 - zjistěte kolik obsahují všechna
-// slova v poli "strings" dohromady písmen - LINQ
-// var sumletters = strings.Select(x => x.Length).Sum();
-// Console.WriteLine($"vsechna slova v poli strings maji dohromady {sumletters} pismen");
-
-// 5 - vytvořte novou kolekci obsahující dvojici
-// lowercase i uppercase variantu
-//var result = strings
-//            .Select(slovo => new UpperLowerString(slovo))
-//            .Select(x => $"upper:{x.UpperCase} lower:{x.LowerCase}");
-
-// - 5 pomoci tuplu
-// var result = strings.Select(slovo => (slovo.ToLower(), slovo.ToUpper()));
-
-// 6 - LINQ - frekvence vyskytu jednotlivych pismen ve vsech
-// polozkach pole strings (kombinovane - v celem poli)
-
-// agregace - https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.aggregate?view=net-6.0
-//var res = strings.Aggregate(
-//   "", // start with empty string to handle empty list case.
-//   (agg, item) => agg + item);
-//Console.WriteLine(res);
-
-//var aggregated = string.Join("", strings); //spojim slova do jednoho retece
-//var result = aggregated // pracuji se stringem jako s kolekci znaku
-//    .GroupBy(x => x) // seskupuji podle pismenek (char v koleci string)
-//    .Select(g => (Letter: g.Key,Count: g.Count())) // udelam tuple obsahujici klic (pismenko) a pocet prvku
-//    .OrderBy(x => x.Count)
-//    .ThenByDescending(x => x.Letter)
-//    ; 
-
-// Dictionary - https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.dictionary-2?view=net-6.0
-
-//var bookdir = @"C:\Users\Student\source\repos\CNET2\Books";
-
-//foreach(var file in GetFilesFromDir(bookdir))
-//{
-//    var dict = TextTools.TextTools.FreqAnalysis(file);
-//    var top10 = TextTools.TextTools.GetTopWords(10, dict);
-
-//    var fi = new FileInfo(file);
-    
-//    Console.WriteLine("KNIHA: " + fi.Name);
-//    PrintList(top10.Select(x => $"{x.Key} : {x.Value}").ToList());
-//    Console.WriteLine();
-//}
+var task1 = Task.Run(() => 
+        {
+            TextTools.TextTools.FreqAnalysisFromFile(@"C:\Users\Student\Documents\BigFiles\words01.txt", Environment.NewLine);
+            Console.WriteLine("Task 1 finished.");
+        });
 
 
+var task2 = Task.Run(() =>
+            {
+                TextTools.TextTools.FreqAnalysisFromFile(@"C:\Users\Student\Documents\BigFiles\words09.txt", Environment.NewLine);
+                Console.WriteLine("Task 2 finished.");
+            });
 
 
+await Task.WhenAny(task1, task2);
+
+
+Console.WriteLine("Program finished.");
 Console.WriteLine();
 
 
@@ -111,4 +63,70 @@ static Dictionary<char, int> CharFreq(string input)
 static IEnumerable<string> GetFilesFromDir(string dir)
 {
     return Directory.EnumerateFiles(dir);
+}
+
+static void LINQ()
+{
+    var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+    var strings = new[] { "zero", "one", "two", "three",
+                        "four", "five", "six", "seven",
+                        "eight", "nine" };
+
+    // 1 strings - pomocí LINQu vytvořte nové pole kde jsou všechna slova uppercase
+    //var result = strings.Select(x => x.ToUpper());
+
+    // 2 numbers - zjiste pomoci LINQu jestli pole obsahuje pouze suda cisla
+    //bool isOnlyEvenNumbers = numbers.All(x => x % 2 == 0);
+    //global::System.Console.WriteLine($"jsou vsechna cisla suda: {isOnlyEvenNumbers}");
+
+    // 3 - vypište čísla v poli numbers jako slova - LINQ
+    // var result = numbers.Select(x => strings[x]);
+
+    // 4 - zjistěte kolik obsahují všechna
+    // slova v poli "strings" dohromady písmen - LINQ
+    // var sumletters = strings.Select(x => x.Length).Sum();
+    // Console.WriteLine($"vsechna slova v poli strings maji dohromady {sumletters} pismen");
+
+    // 5 - vytvořte novou kolekci obsahující dvojici
+    // lowercase i uppercase variantu
+    //var result = strings
+    //            .Select(slovo => new UpperLowerString(slovo))
+    //            .Select(x => $"upper:{x.UpperCase} lower:{x.LowerCase}");
+
+    // - 5 pomoci tuplu
+    // var result = strings.Select(slovo => (slovo.ToLower(), slovo.ToUpper()));
+
+    // 6 - LINQ - frekvence vyskytu jednotlivych pismen ve vsech
+    // polozkach pole strings (kombinovane - v celem poli)
+
+    // agregace - https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.aggregate?view=net-6.0
+    //var res = strings.Aggregate(
+    //   "", // start with empty string to handle empty list case.
+    //   (agg, item) => agg + item);
+    //Console.WriteLine(res);
+
+    //var aggregated = string.Join("", strings); //spojim slova do jednoho retece
+    //var result = aggregated // pracuji se stringem jako s kolekci znaku
+    //    .GroupBy(x => x) // seskupuji podle pismenek (char v koleci string)
+    //    .Select(g => (Letter: g.Key,Count: g.Count())) // udelam tuple obsahujici klic (pismenko) a pocet prvku
+    //    .OrderBy(x => x.Count)
+    //    .ThenByDescending(x => x.Letter)
+    //    ; 
+
+    // Dictionary - https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.dictionary-2?view=net-6.0
+
+    //var bookdir = @"C:\Users\Student\source\repos\CNET2\Books";
+
+    //foreach(var file in GetFilesFromDir(bookdir))
+    //{
+    //    var dict = TextTools.TextTools.FreqAnalysis(file);
+    //    var top10 = TextTools.TextTools.GetTopWords(10, dict);
+
+    //    var fi = new FileInfo(file);
+
+    //    Console.WriteLine("KNIHA: " + fi.Name);
+    //    PrintList(top10.Select(x => $"{x.Key} : {x.Value}").ToList());
+    //    Console.WriteLine();
+    //}
 }
