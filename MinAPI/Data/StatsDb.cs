1ed2ad951417388407a0c63aa7116a6a7874bb84
kubicek-skoleni <kubicek.brno@gmail.com>
2021-11-25 13:33:05 +0100

    StatsDb

---
+++ MinAPI/Data/StatsDb.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Collections.Generic;
+using WPFTextGUI.Model;
+
+namespace MinAPI.Data
+{
+    public class StatsDb : DbContext
+    {
+        public StatsDb(DbContextOptions<StatsDb> options) : base(options) { }
+
+        public DbSet<StatsResult> StatsResults => Set<StatsResult>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<StatsResult>()
+                .Property(b => b.Top10Words)
+                .HasConversion(
+                   v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                   v => JsonSerializer.Deserialize<Dictionary<string, int>>(v, (JsonSerializerOptions)null));
+        }
+    }
+}
