5c64d840c59c6d56993e70a8b22bf2be7fff481e
kubicek-skoleni <kubicek.brno@gmail.com>
2021-11-25 12:00:02 +0100

    minimal api dummy // POST -> /stats // GET -> /stats/5 // GET -> /stats/all

--- MinAPI/Program.cs
+++ MinAPI/Program.cs
@@ -24,6 +24,12 @@ app.MapGet("/hello", () => "hello");
 // GET -> /stats/5
 // GET -> /stats/all
 
+app.MapPost("/stats", (StatsResult result) =>
+{
+    return "ok";
+});
+
+
 app.MapGet("/stats/{id}", (int id) =>
 {
     StatsResult sr = new StatsResult();
@@ -32,6 +38,16 @@ app.MapGet("/stats/{id}", (int id) =>
     return sr;
 });
 
+app.MapGet("/stats/all", GetAllResults);
 
 app.Run();
 
+static List<StatsResult> GetAllResults()
+{
+    return new List<StatsResult>()
+    {
+        new StatsResult() { Id = 1, Source = "dummy result"},
+        new StatsResult() { Id = 2, Source = "dummy result"},
+        new StatsResult() { Id = 3, Source = "dummy result"}
+    };
+}
