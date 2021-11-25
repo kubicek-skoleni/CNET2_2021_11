12fa99a04795629608af9bdd5c79ff883213979c
kubicek-skoleni <kubicek.brno@gmail.com>
2021-11-25 14:40:41 +0100

    init migration + update database

---
+++ MinAPI/Migrations/20211125132525_init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinAPI.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StatsResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Source = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ElapsedMilliseconds = table.Column<int>(type: "INTEGER", nullable: false),
+                    Top10Words = table.Column<string>(type: "TEXT", nullable: false),
+                    SubmitedBy = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatsResults", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StatsResults");
+        }
+    }
+}
